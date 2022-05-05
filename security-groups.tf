fde1ccf9b0460456a71cae3c052b9cb0095a21e0
Delorrres <deloresmawuse@gmail.com>
2022-05-04 23:49:50 -0500

    UPDATE- fix error

--- security-groups.tf
+++ security-groups.tf
@@ -1,7 +1,8 @@
 resource "aws_security_group" "my_private_app_sg" {
-  name        = "Logstash_sg"
+  name        = "syslog_sg"
   description = "Allow access to this server"
   vpc_id      = data.aws_vpc.main.id
+
 
   # INBOUND CONNECTIONS
   ingress {
@@ -12,21 +13,13 @@ resource "aws_security_group" "my_private_app_sg" {
     cidr_blocks = ["192.168.0.0/16"] # 0.0.0.0/0
   }
 
-  #Logstash connectiom
+#Logstash connectiom
   ingress {
-    description = "Allow demo servers to send logs to Logstash"
+    description = "Allow dummy server to send logs to Logstash"
     from_port   = 5044
     to_port     = 5044
     protocol    = "tcp"
     cidr_blocks = ["192.168.0.0/16"] # 0.0.0.0/0
   }
 
-  # OUTBOUT CONNECTIONS
-  egress {
-    description = "Allow access to the world"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # TCP + UDP
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
--- storage.tf
+++ storage.tf
@@ -1,10 +1,10 @@
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.logstash_storage.id
-  instance_id = aws_instance.logstash.id
+  volume_id   = aws_ebs_volume.syslog_storage.id
+  instance_id = aws_instance.syslog.id
 }
 
-resource "aws_ebs_volume" "logstash_storage" {
+resource "aws_ebs_volume" "syslog_storage" {
   availability_zone = "us-east-2a"
   size              = 50
 }
